24b83dcf107559eec898bf47c1b29faf66ba631d
Devyn <devyn.hedin@outlook.com>
2018-04-24 14:47:03 -0700

    Tracking kinda works

---
+++ Assets/KeepScore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepScore : MonoBehaviour {
+
+	public int playerScore;
+	public int aiScore;
+	private GameObject ballObject;
+	public BallController bController;
+
+	// Use this for initialization
+	void Start () {
+		playerScore = 0;
+		aiScore = 0;
+		ballObject = GameObject.Find ("Ball");
+
+	}
+	
+	void Update () {
+		GetComponent<TextMesh> ().text = "AI Score: " + aiScore.ToString() + "\nPlayer Score: " + playerScore.ToString();
+		if (ballObject.transform.position.z < -26) {
+			aiScore += 1;
+			ballObject.transform.position = new Vector3 (0f, 0f, 0f);
+			bController.startGame ();
+		}
+		if (ballObject.transform.position.z > 26) {
+			playerScore += 1;
+			ballObject.transform.position = new Vector3 (0f, 10f, 0f);
+			bController.startGame ();
+		}
+	}
+}
---
+++ Assets/TestScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	public AndroidJavaClass jc;
+	public TextMesh textObject;
+	public string deltaString;
+
+	public Vector3 pos;
+
+	public Vector3 origin;
+
+	// Use this for initialization
+	void Start () {
+		origin = new Vector3 (3f, 4.53f, -18f);
+		jc = new AndroidJavaClass ("com.devynhedin.receiver.MyReceiver");
+		jc.CallStatic ("createInstance");
+		textObject = GameObject.Find("BluetoothTextTest").GetComponent<TextMesh>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		deltaString = jc.GetStatic<string> ("delta");
+		textObject.text = deltaString;
+
+		string[] arr = deltaString.Split('\t');
+
+		GameObject.Find ("Coordinate-1").GetComponent<TextMesh> ().text = arr [3] + " | " + (double.Parse(arr[3]) / double.Parse(arr[6]));
+		GameObject.Find ("Coordinate-2").GetComponent<TextMesh> ().text = arr [4] + " | " + (double.Parse(arr[4]) / double.Parse(arr[6]));
+		GameObject.Find ("Coordinate-3").GetComponent<TextMesh> ().text = arr [5] + " | " + (double.Parse(arr[5]) / double.Parse(arr[6]));
+		GameObject.Find ("Coordinate-4").GetComponent<TextMesh> ().text = arr [6];
+
+		double coordinate_1 = double.Parse (arr [3]);
+		double coordinate_2 = double.Parse (arr [4]);
+		double coordinate_3 = double.Parse (arr [5]);
+		double coordinate_4 = double.Parse (arr [6]);
+
+		pos = GameObject.Find ("Main Camera").transform.position;
+
+		pos.x = (float)(coordinate_1 / coordinate_4) + origin.x;
+		pos.y = (float)(coordinate_2 / coordinate_4) + origin.y;
+		pos.z = (float)(coordinate_3 / coordinate_4) + origin.z;
+
+		GameObject.Find ("Main Camera").transform.position = pos;
+
+	}
+}
